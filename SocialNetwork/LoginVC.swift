a1e07a9452ab25d8ee1e3a3ac31257eab7bcc62e
Shruti Sharma <shruti25sharma@gmail.com>
2018-03-05 16:01:48 -0800

    Manage keyboard on textfield tap event handled and email authentication with Firebase implemented

--- SocialNetwork/LoginVC.swift
+++ SocialNetwork/LoginVC.swift
@@ -13,21 +13,31 @@ import FirebaseAuth
 
 class LoginVC: UIViewController {
 
+    //MARK: - Class member variables declaration
+
     @IBOutlet weak var pswdTextField: CustomTextField!
     @IBOutlet weak var emailTextField: CustomTextField!
     @IBOutlet weak var emailErrorLbl: UILabel!
     @IBOutlet weak var pswdErrorLbl: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    var isKeyboardVisible = false
 
     //MARK: - View Lifecycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpLoginView()
+        registerForKeyboardNotifications()
+        addTapGestureOnScrollView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
     /* //Code to add Facebook Default Login Button
@@ -51,14 +61,56 @@ class LoginVC: UIViewController {
         
         /* Your app can only have one person logged in at a time. We represent each person logged into your app with AccessToken.current. The LoginManager of Facebook SDK sets this token for you and when it sets current it also automatically writes it to a keychain cache. The AccessToken contains userId which is used to identify the user.
          */
-        if let accessToken = AccessToken.current {
-            // User is logged in, use 'accessToken' here.
-        }
-        else {
-        }
+//        if let accessToken = AccessToken.current {
+//            // User is logged in, use 'accessToken' here.
+//        }
+//        else {
+//        }
+    }
+    
+    //MARK: - Keyboard Show/Hide Notifications Handling
 
+    func registerForKeyboardNotifications() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        
+        let userInfo = notification.userInfo!
+        
+        //1. When working with keyboards, the dictionary will contain a key called 'UIKeyboardFrameEndUserInfoKey' telling us the frame of the keyboard after it has finished animating. This will be of type NSValue, which in turn is of type CGRect
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        //2. Here we convert the keyboard frame to our view's co-ordinates. This is because rotation isn't factored into the frame, so if the user is in landscape we'll have the width and height flipped – using the convert() method will fix that.
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        //3. This code adjusts the bottom content inset of the scroll view by the height of the keyboard. It also sets the scrollIndicatorInsets property of the scroll view to the same value so that the scrolling indicator won’t be hidden by the keyboard.
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+            isKeyboardVisible = false
+            scrollView.contentInset = UIEdgeInsets.zero
+            scrollView.scrollIndicatorInsets = scrollView.contentInset
+        } else if !isKeyboardVisible{
+            isKeyboardVisible = true
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0) ////we use the property contentInset to add more scrollable space at the bottom
+            scrollView.scrollIndicatorInsets = scrollView.contentInset
+        }
     }
 
+    //MARK: - ScrollView Touches Handling
+
+    func addTapGestureOnScrollView() {
+      
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(scrollViewTapped))
+        tapGesture.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func scrollViewTapped() {
+        self.view.endEditing(true)
+    }
     
     //MARK: - Login Button Tap Events
 
@@ -66,14 +118,54 @@ class LoginVC: UIViewController {
         
         guard let email = emailTextField.text, let pswd = pswdTextField.text else { return}
         
-        checkEmailAddressValidation()
-        checkPasswordValidation()
+        let emailValidationStatus = checkEmailAddressValidation()
+        let pswdValidationStatus = checkPasswordValidation()
         
-//        Auth.auth().signIn(withEmail: email, password: pswd) { (user, error) in
-//            // ...
-//        }
+        if !emailValidationStatus || !pswdValidationStatus {
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: pswd) { (user, error) in
+            
+            if error != nil {
+                if let errCode = AuthErrorCode(rawValue: error!._code) {
+                    switch errCode {
+                    case .userNotFound:
+                        print("this email does not exist so first create an account for this user")
+                        self.createAccountForUser(withEmail: email, pswd: pswd)
+                    case .wrongPassword:
+                        print("Sorry, either your e-mail or password didn't match what we have on file. Try it again?" )
+                    default:
+                        print("Sign-In User Error: \(error!)")
+                    }
+                    
+                }
+            }
+            else {
+                if let userObj = user {
+                    print("user name: \(String(describing: userObj.displayName))")
+                    print("user email: \(String(describing: userObj.email))")
+                }
+            }
+        }
     }
     
+    func createAccountForUser(withEmail email: String, pswd: String) {
+    
+        Auth.auth().createUser(withEmail: email, password: pswd, completion: { (user, error) in
+            if error == nil {
+                print("New User account created and signed-in successfuly")
+                print("user name: \(String(describing: user?.displayName))")
+                print("user email: \(String(describing: user?.email))")
+            }
+            else {
+                if let errCode = AuthErrorCode(rawValue: error!._code) {
+                    print("Create User error: \(errCode)")
+                }
+            }
+        })
+
+    }
     
     @IBAction func facebookLoginBtnTapped(_ sender: Any) {
         
@@ -93,13 +185,13 @@ class LoginVC: UIViewController {
                  the facebook access token for the signed-in user is used to create a Firebase credential
                  */
                 let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
-                self.firebaseAuth(credential)
+                self.firebaseAuthWithFacebook(credential)
             }
         }
     }
 
-    //MARK: - Authenticate with Firebase
-    func firebaseAuth(_ credential: AuthCredential) {
+    //MARK: - Authenticate Facebook Credential with Firebase
+    func firebaseAuthWithFacebook(_ credential: AuthCredential) {
         
         // authenticate with Firebase using the Firebase credential
         Auth.auth().signIn(with: credential) { (user, error) in
@@ -129,15 +221,19 @@ class LoginVC: UIViewController {
         return emailTest.evaluate(with: email)
     }
 
-    func checkEmailAddressValidation() {
+    func checkEmailAddressValidation() -> Bool {
+        
+        emailTextField.resignFirstResponder()
         if !isValidEmail(emailTextField.text!) {
             emailErrorLbl.text = "Please enter a vaild email address."
             emailErrorLbl.isHidden = false
             emailTextField.changeBorderColor(isError: true)
+            return false
         }
         else {
             emailErrorLbl.isHidden = true
             emailTextField.changeBorderColor(isError: false)
+            return true
         }
     }
     
@@ -152,17 +248,22 @@ class LoginVC: UIViewController {
         return passwordTest.evaluate(with: pswd)
     }
     
-    func checkPasswordValidation() {
+    func checkPasswordValidation() -> Bool{
+        
+        pswdTextField.resignFirstResponder()
         if !isValidPassword(pswdTextField.text!) {
             pswdErrorLbl.text = "Password must be 8-20 characters with at least 1 uppercase and lowercase letters and 1 digit."
             pswdErrorLbl.isHidden = false
             pswdTextField.changeBorderColor(isError: true)
+            return false
         }
         else {
             pswdErrorLbl.isHidden = true
             pswdTextField.changeBorderColor(isError: false)
+            return true
         }
     }
+    
 }
 
 
@@ -186,19 +287,12 @@ extension LoginVC: LoginButtonDelegate {
              the facebook access token for the signed-in user is used to create a Firebase credential
             */
             let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
-            firebaseAuth(credential)
+            firebaseAuthWithFacebook(credential)
         }
     }
 
     func loginButtonDidLogOut(_ loginButton: LoginButton) {
         
-    }
-    
-    /**
-     * Called when the user click on the view (outside the UITextField).
-     */
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       self.view.endEditing(true)
     }
     
 }
@@ -207,12 +301,8 @@ extension LoginVC: LoginButtonDelegate {
 
 extension LoginVC: UITextFieldDelegate {
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        return true
-    }
-    
     func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.resignFirstResponder()
+
         /*
         if textField == emailTextField {
             updateViewOnEmailValidation()
